9194ddec79970cf60c58e79effa1271d9a07ad44
ZoleeG <peter88.ujlaki@gmail.com>
2024-10-03 07:00:46 +0100

    Initializing project

---
+++ DriverInfo.API/Controllers/DriversController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace DriverInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/drivers")]
+    public class DriversController : ControllerBase
+    {
+        [HttpGet()]
+        public JsonResult GetDrivers()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new{ id = 1, Name = "Max Verstappen"},
+                    new{ id = 2, Name = "Lewis Hamilton"}
+                });
+        }
+    }
+}
